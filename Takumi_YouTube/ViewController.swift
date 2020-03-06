61dcff62e5575820bf9e91020d15258e2a553b27
Mitsuki Goto <minionnzu20041125@yahoo.co.jp>
2020-03-06 12:10:08 +0900

    Initalized Commit

---
+++ Takumi_YouTube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Takumi_YouTube
+//
+//  Created by MitsukiGoto on 2020/03/06.
+//  Copyright © 2020 MitsukiGoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Takumi_YouTubeTests/Takumi_YouTubeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Takumi_YouTubeTests.swift
+//  Takumi_YouTubeTests
+//
+//  Created by MitsukiGoto on 2020/03/06.
+//  Copyright © 2020 MitsukiGoto. All rights reserved.
+//
+
+import XCTest
+@testable import Takumi_YouTube
+
+class Takumi_YouTubeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
